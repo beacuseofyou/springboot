dd1e162446e7d70453e596d1ca651e20be6efefc
beacuseofyou <1158388143@qq.com>
2018-08-28 17:44:29 +0800

    整合redis，加入哨兵模式

--- springboot-redis/src/main/java/com/controller/StudentController.java
+++ springboot-redis/src/main/java/com/controller/StudentController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @description:学生Controller
  * @author:San Jinhong
@@ -19,6 +22,17 @@ public class StudentController {
 
     @RequestMapping("/findStudentById")
     public Object findAllStudents(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                studentService.findStudentById(2);
+            }
+        };
+        //多线程测试一下缓存穿透问题
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
+        for(int i = 0; i < 10000; i++){
+            executorService.submit(runnable);
+        }
         return studentService.findStudentById(1);
     }
 }
