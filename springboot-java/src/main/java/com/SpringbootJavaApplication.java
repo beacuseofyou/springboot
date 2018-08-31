37cf823cbac052ed2059580dac8e8e2fc7060c3c
timefireworks <1158388143@qq.com>
2018-08-31 15:20:18 +0800

    springboot开发非web程序方式一

--- springboot-java/src/main/java/com/SpringbootJavaApplication.java
+++ springboot-java/src/main/java/com/SpringbootJavaApplication.java
@@ -1,12 +1,21 @@
 package com;
 
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootJavaApplication {
 
+    @Autowired
+    private UserService userService;
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootJavaApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootJavaApplication.class, args);
+        UserService userService = (UserService) context.getBean("userServiceImpl");
+        String s = userService.sayHi("xxx");
+        System.out.println(s);
     }
 }
