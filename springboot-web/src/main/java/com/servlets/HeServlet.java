e4006b60082a572d8f0c448779deba890bc18eb5
timefireworks <1158388143@qq.com>
2018-08-31 14:49:27 +0800

    springboot-web

---
+++ springboot-web/src/main/java/com/servlets/HeServlet.java
@@ -0,0 +1,24 @@
+package com.servlets;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("he servlet hello world");
+        response.getWriter().flush();
+        response.getWriter().close();
+    }
+}
