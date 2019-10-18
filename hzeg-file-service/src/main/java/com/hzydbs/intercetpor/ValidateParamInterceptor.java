41665970fec5639486db174e135aecb689195776
sjh <1158388143@qq.com>
2019-10-18 17:48:39 +0800

    file

---
+++ hzeg-file-service/src/main/java/com/hzydbs/intercetpor/ValidateParamInterceptor.java
@@ -0,0 +1,15 @@
+package com.hzydbs.intercetpor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * author: San Jinhong
+ * date: 2019/10/18 9:22
+ **/
+@Component
+public class ValidateParamInterceptor implements HandlerInterceptor {
+
+
+
+}
